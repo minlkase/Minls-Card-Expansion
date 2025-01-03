3f61ac10b8ff91093325d05c46335ec6833508ff
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-01-03 10:19:17 +0100

    .

--- script/900000001.lua
+++ script/900000001.lua
@@ -3,7 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -11,9 +10,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x105}
+s.listed_series={0x2f}
 function s.filter(c)
-	return c:IsSetCard(0x105) and c:IsAbleToHand()
+	return c:IsSetCard(0x2f) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
