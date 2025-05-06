0d31b6d23a45eca5b4a95cfc8e479f7f42524a5b
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-05-06 20:44:39 +0200

    fix

--- script/c900000007.lua
+++ script/c900000007.lua
@@ -54,7 +54,7 @@ end
 function s.sfdtarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,0)
 	local ct=#g-Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.sfdfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and ct<=0 end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sfdfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and ct<0 end
     Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.sfd(e,tp,eg,ep,ev,re,r,rp)
