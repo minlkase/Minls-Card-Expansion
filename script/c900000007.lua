df451973932f0cdd1fccbe3f4009913b9e557ba7
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-05-06 19:09:10 +0200

    tenpai

---
+++ script/c900000007.lua
@@ -0,0 +1,126 @@
+--Sangen Ina:
+local s,id=GetID()
+--[[
+If your opponent controlls more cards than you do: Special Summon 1 FIRE Dragon monster from your Deck, then discard 1 card,
+also you cannot Special Summon monsters for the rest of this Duel, except "Tenpai Dragon" monsters or Dragon Type monsters from the Extra Deck.
+If this card is in your GY, you can banish 1  "Sangen" Spell/Trap and 1 "Tenpai Dragon" monster (1 from the hand and 1 from the Deck);
+add this card from your GY to your hand.
+You can only use this effect of "Sangen Ina" once per Turn.
+During the BP, you can banish this card form your GY (QE); Special Summon 1 FIRE Dragon monster from your GY, then discard 1 card.
+You can only use this effect of "Sangen Ina" once per Duel.
+--]]
+
+function s.initial_effect(c)
+    --Special from Deck
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.sfdtarget)
+	e1:SetOperation(s.sfd)
+	c:RegisterEffect(e1)
+
+    --to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+    e2:SetCountLimit(1,{id,0})
+	c:RegisterEffect(e2)
+
+    --BP banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_MAIN_END|TIMING_BATTLE_START|TIMING_ATTACK|TIMING_BATTLE_PHASE)
+	e3:SetCountLimit(1)
+	e3:SetCost(Cost.SelfBanish)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+
+
+end
+s.listed_series={SET_SANGEN}
+
+
+function s.sfdfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sfdtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	local ct=#g-Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sfdilter,tp,LOCATION_DECK,0,1,nil) and ct<=0 end
+    Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.sfd(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.sfdilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if #g>0 and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT|REASON_DISCARD,nil)
+	end
+    local ge1=Effect.CreateEffect(c)
+	ge1:SetType(EFFECT_TYPE_FIELD)
+	ge1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	ge1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	ge1:SetTargetRange(1,0)
+	ge1:SetTarget(s.splimit)
+	Duel.RegisterEffect(ge1,tp)
+
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+    return not ((c:IsLocation(LOCATION_EXTRA) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON)) or c:IsSetCard(SET_TENPAI_DRAGON))
+end
+
+function s.bfdgfilter1(c)
+	return c:IsSetCard(SET_TENPAI_DRAGON) and c:IsMonster() and c:IsAbleToRemoveAsCost()
+end
+function s.bfdgfilter2(c)
+    return c:IsSetCard(SET_SANGEN) and c:IsSpellTrap() and c:IsAbleToRemoveAsCost()
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    cond1 = Duel.IsExistingMatchingCard(s.bfdgfilter1,tp,LOCATION_HAND,0,1,nil) and Duel.IsExistingMatchingCard(s.bfdgfilter2,tp,LOCATION_DECK,0,1,nil)
+    cond2 = Duel.IsExistingMatchingCard(s.bfdgfilter2,tp,LOCATION_HAND,0,1,nil) and Duel.IsExistingMatchingCard(s.bfdgfilter1,tp,LOCATION_DECK,0,1,nil)
+	if chk==0 then return cond1 or cond2 end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g1=Duel.SelectMatchingCard(tp,function (c) return s.bfdgfilter1(c) or bfdgfilter2(c) end,tp,LOCATION_HAND,0,1,1,nil)
+    if g1:IsSetCard(SET_TENPAI_DRAGON) then
+        f=s.bfdgfilter2
+    elseif g1:IsSetCard(SET_SANGEN) then
+        f=s.bfdgfilter1
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g2=Duel.SelectMatchingCard(tp,f,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.Remove(g1,POS_FACEUP,REASON_COST)
+    Duel.Remove(g2,POS_FACEUP,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end
+
+function s.spfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and uel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
+        Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT|REASON_DISCARD,nil)
+    end
+end
---
+++ script/c900000008.lua
@@ -0,0 +1,107 @@
+--Tenpai Dragon Condra
+local s,id=GetID()
+function s.initial_effect(c)
+--[[
+If this card is Normal or Special Summoned; add 1 Level 4 or lower FIRE Dragon monster from your Deck to your hand.
+You can only use this effect of "Tenpai Dragon Condra" once per turn.
+Your opponent cannot activate cards or effects durring the MP2 and EP.
+Cards and effects activated by your opponent during the BP are negated, while you control more FIRE Dragon monsters, than cards your opponent controlls.
+Once per turn during the BP, you can (QE); Immediatly after this effect resolves, Synchro Summon using cards you control.
+--]]
+
+--add 1 Level 4 or lower FIRE Dragon
+local e1=Effect.CreateEffect(c)
+e1:SetDescription(aux.Stringid(id,0))
+e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+e1:SetProperty(EFFECT_FLAG_DELAY)
+e1:SetCode(EVENT_SUMMON_SUCCESS)
+e1:SetCountLimit(1,id)
+e1:SetTarget(s.thtg)
+e1:SetOperation(s.thop)
+c:RegisterEffect(e1)
+local e2=e1:Clone()
+e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+c:RegisterEffect(e2)
+
+--cannot activate cards or effects durring the MP2 and EP
+local e3=Effect.CreateEffect(c)
+e3:SetType(EFFECT_TYPE_FIELD)
+e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+e3:SetRange(LOCATION_MZONE)
+e3:SetTargetRange(0,1)
+e3:SetCondition(function() return Duel.IsPhase(0x100) or Duel.IsPhase(0x200) end)
+e3:SetValue(1)
+c:RegisterEffect(e3)
+
+--during the BP are negated
+local e4=Effect.CreateEffect(c)
+e4:SetType(EFFECT_TYPE_FIELD)
+e4:SetRange(LOCATION_MZONE)
+e4:SetCondition(s.condition)
+e4:SetTargetRange(0,1)
+e4:SetTarget(s.disable)
+e4:SetCode(EFFECT_DISABLE)
+c:RegisterEffect(e4)
+
+--quick synchro
+local e5=Effect.CreateEffect(c)
+e5:SetDescription(aux.Stringid(id,1))
+e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+e5:SetType(EFFECT_TYPE_QUICK_O)
+e5:SetCode(EVENT_FREE_CHAIN)
+e5:SetRange(LOCATION_MZONE)
+e5:SetHintTiming(0,TIMING_BATTLE_START|TIMING_BATTLE_END)
+e5:SetCountLimit(1)
+e5:SetCondition(function() return Duel.IsBattlePhase() end)
+e5:SetTarget(s.synchtg)
+e5:SetOperation(s.synchop)
+c:RegisterEffect(e5)
+
+end
+s.listed_series={SET_TENPAI_DRAGON}
+
+
+function s.thfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.ShuffleHand(tp)
+		Duel.BreakEffect()
+	end
+end
+
+function s.disable(e,c)
+	return c:IsType(TYPE_EFFECT) or (c:GetOriginalType()&TYPE_EFFECT)==TYPE_EFFECT
+end
+function s.nfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON)
+end
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	g=Duel.GetMatchingGroup(s.nfilter,tp,LOCATION_ONFIELD,0,nil)
+	return #g-Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)>0
+end
+
+function s.synchtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.synchop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsControler(1-tp) or not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,c)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SynchroSummon(tp,sg:GetFirst(),c)
+	end
+end
