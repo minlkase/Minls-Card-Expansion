d48f9942585a9fc06c8c456dbc083d3f85b21e6f
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-05-06 19:17:10 +0200

    fix

--- script/c900000007.lua
+++ script/c900000007.lua
@@ -112,7 +112,7 @@ function s.spfilter(c)
 	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and uel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and uel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
