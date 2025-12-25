d4ab97d2d466757c8572199138c40a6e5537df15
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-12-25 02:55:51 +0100

    mitsu alemb wip

---
+++ script/c900000010.lua
@@ -0,0 +1,19 @@
+--Primitsurugi Alembertian
+local s,id=GetID()
+function s.initial_effect(c)
+	--[[
+		☆ You can Tribute 1 Monster; Special Summon 1 Lever 4 "Mitsurugi" Monster from your Hand or GY.
+		☆ If this card is Tributed: You can add 1 "Mitsurugi" card from your Deck to your hand,
+		then you can Special Summon this card.
+		☆ If this card is Xyz Summoned: You can detach 2 to 4 materials from this card, then activate the appropriate effect;
+		●2: Add 1 "Mitsurugi" card from your Deck to your Hand.
+		●3: Ritual Summon 1 Reptile Ritual Monster from your Hand, by Tributing up to 2 Reptile monsters
+		from your Hand, Deck, or field, whose total Levels equal the Level of the Ritual Monster.
+		●4: Ritual Summon 1 Reptile Ritual Monster from your Deck, GY or Banished, by Tributing up to 2 Reptile monsters
+		from your Hand, Deck, or field, whose total Levels equal the Level of the Ritual Monster. 
+		You can only use each effect of "Primitsurugi Alembertial" once per turn.
+	--]]
+
+	
+end
+s.listed_series={SET_MITSURUGI}
