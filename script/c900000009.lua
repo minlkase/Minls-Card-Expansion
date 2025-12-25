88b3fd4dcd6f6ccf40a30f78eec8084adbdb024f
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-12-25 02:22:25 +0100

    changes

--- script/c900000009.lua
+++ script/c900000009.lua
@@ -115,7 +115,7 @@ function s.tributelimit(e,tp,g,sc)
 	return #g<=2,#g>2
 end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local params1={handler=c,lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,extraop=s.extraop,stage2=s.effsh,forcedselection=s.tributelimit}
+	local params1={handler=c,lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_HAND|LOCATION_MZONE|LOCATION_GRAVE|LOCATION_REMOVED,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,extraop=s.extraop,stage2=s.effsh,forcedselection=s.tributelimit}
 	if chk==0 then return not Duel.HasFlagEffect(tp,id) and Ritual.Target(params1)(e,tp,eg,ep,ev,re,r,rp,0) end
 	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
