1cafc4190171d180ab04401df5db400162ee865c
tihe810f <till_georg.hermann@mailbox.tu-dresden.de>
2025-12-26 15:40:10 +0100

    mitsu alemb wip

--- script/c900000010.lua
+++ script/c900000010.lua
@@ -137,6 +137,7 @@ function s.tributelimit(e,tp,g,sc)
 end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local op=e:GetLabel()
+	Debug.Message("op: "..op)
 	local params1={handler=c,lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_HAND,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,extraop=s.extraop,forcedselection=s.tributelimit}
 	local params2={handler=c,lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,extraop=s.extraop,forcedselection=s.tributelimit}
 	if chk==0 then return (op==1 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)) or (op==2 and not Duel.HasFlagEffect(tp,id) and Ritual.Target(params1)(e,tp,eg,ep,ev,re,r,rp,0)) or (op==3 and not Duel.HasFlagEffect(tp,id) and Ritual.Target(params2)(e,tp,eg,ep,ev,re,r,rp,0)) end
